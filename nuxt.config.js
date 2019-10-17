2698f645c96e926967c59052c0d8794ccff95928
GamesProSeif <seifmansour2014@outlook.com>
2019-10-17 03:16:09 +0300

    Change title and description

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,11 +4,11 @@ module.exports = {
   ** Headers of the page
   */
 	head: {
-		title: process.env.npm_package_name || '',
+		title: 'Biology Graduation Party',
 		meta: [
 			{ charset: 'utf-8' },
 			{ name: 'viewport', content: 'width=device-width, initial-scale=1' },
-			{ hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+			{ hid: 'description', name: 'description', content: 'Website for Mrs Mona\'s graduation party' }
 		],
 		link: [
 			{ rel: 'icon', type: 'image/x-icon', href: '/favicon.png' }
